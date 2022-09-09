b0e207800c16e37e52d94425a562cd82b242a27d
Santos82 <ydaniel353@gmail.com>
2022-09-08 20:00:55 -0700

    Task 4-6 level c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Printing
+ * 
+ * Return: Always 0(Sucessful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a mutilingual puzzle");
+	return (0);
+}
