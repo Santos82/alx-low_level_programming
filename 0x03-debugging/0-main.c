7dec2682dc45bbddbdcb75514991bc852ddbfd9d
Santos82 <ydaniel353@gmail.com>
2022-09-16 13:51:51 -0700

    Task 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,21 +1,18 @@
 #include "main.h"
 
-
 /**
  * main - prints positive or negative interger
  * Return: 0
  */
-
 
 int main(void)
 {
 
 	        int i;
 
+		 i =0;
 
-		 i = 98;
 		 positive_or_negative(i);
-		
 
 		 return (0);
 
